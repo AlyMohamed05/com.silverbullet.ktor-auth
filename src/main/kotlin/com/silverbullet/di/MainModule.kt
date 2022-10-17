a432bf374f90b3ae007eb29a534b1ce8fbf6e5b4
Aly <asecure05@gmail.com>
2022-10-17 21:29:04 +0200

    Basic Dependency Injection with Koin.

---
+++ src/main/kotlin/com/silverbullet/di/MainModule.kt
@@ -0,0 +1,16 @@
+package com.silverbullet.di
+
+import org.koin.dsl.module
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
+
+val mainModule = module {
+
+    single {
+        KMongo
+            .createClient()
+            .coroutine
+            .getDatabase("ktor-auth")
+    }
+
+}
