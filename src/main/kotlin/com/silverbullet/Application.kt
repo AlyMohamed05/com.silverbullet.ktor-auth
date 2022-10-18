7a195b1a3044efee2d609a3d66a1f1aef5217e2d
Aly <asecure05@gmail.com>
2022-10-18 13:55:19 +0200

    Security plugin modifications

--- src/main/kotlin/com/silverbullet/Application.kt
+++ src/main/kotlin/com/silverbullet/Application.kt
@@ -2,15 +2,24 @@ package com.silverbullet
 
 import io.ktor.server.application.*
 import com.silverbullet.plugins.*
+import com.silverbullet.security.token.TokenConfig
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
+
+    val tokenConfig = TokenConfig(
+        issuer = environment.config.property("jwt.issuer").getString(),
+        audience = environment.config.property("jwt.audience").getString(),
+        expiresIn = 31L * 24L * 60L * 60L * 1000L,
+        secret = System.getenv("JWT_SECRET")
+    )
+
     configureKoin()
     configureMonitoring()
     configureSerialization()
-    configureSecurity()
+    configureSecurity(tokenConfig)
     configureRouting()
 }
